5e32d96c26d6bdeeb6652007811801b9264ba4e4
Sam Soutor <43799212+SamSoutor@users.noreply.github.com>
2019-04-18 19:26:37 -0500

    Fixed typo and added more shapes to code

--- Nurse Pals/src/nurse/pals/Drug.java
+++ Nurse Pals/src/nurse/pals/Drug.java
@@ -4,16 +4,10 @@ public class Drug {
 	private String name;
 	private String strenght;
 	private String color;
-	private String[] shape = {"Round","Oblong", "Oval","Square","Rectangle","Diamnond"};
+	private String[] shape = {"Round","Oblong", "Oval","Square","Rectangle","Diamond", "Triangle", "Prism"};
 	//National Drug Code
 	private long ndc;
 	private boolean brand;
 	private String description;
 	private String[] category =  {"OTC", "IV", "RX" } ;
-	
-	
-	
-	
-	
-
 }
